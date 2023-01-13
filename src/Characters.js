ba33642a085eddc47d77d75cdcc8c4583325c7e7
neerajkumar790652 <92433406+neerajkumar790652@users.noreply.github.com>
2023-01-13 14:04:03 +0530

    Update Characters.js

--- src/Characters.js
+++ src/Characters.js
@@ -1,4 +1,4 @@
 export const numbers ='0123456789';
 export const upperCaseLetter ='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 export const lowerCaseLetter = 'abcdefghijklmnopqrstuvwxyz';
-export const specialCharacters =`~!@#$%^&*()_+/*-?><:}{][\|;.,"";`
+export const specialCharacters =`~!@#$%^&*()_+/*-?><:}{][;.,"";`
